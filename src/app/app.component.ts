fbc8baec989282ee3f4bfd8df9e1bbbdebce4204
vinay kumar maurya <v7naym@gmail.com>
2021-01-13 12:08:02 +0530

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CoronaService} from './services/corona.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  countries:any;
+  country:any;
+  confirmed:Number;
+  recovered:Number;
+  deaths:Number;
   title = 'covidInfo';
+  constructor(private corona:CoronaService){}
+
+  ngOnInit(){
+       this.corona.getCountries().subscribe((data)=>{
+      console.log(data)
+      this.countries = data;
+    })
+  }
+
+  getCountry(country:any){
+    this.country=country;
+
+  }
+  getCoronaData(){
+    this.corona.getCoronaRealTimeData(this.country).subscribe((data)=>{
+      console.log(data)
+      let index= data.length-1
+      this.confirmed = data[index].Confirmed
+      this.recovered =data[index].Recovered
+      this.deaths=data[index].Deaths
+    })
+
+  }
 }
